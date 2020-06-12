fdfde2993d11d6a61f61cfe2fe5e71b6c36a4f3f
Kseniia Dudnyk <Kseniia.Dudnyk@innovecs.com>
2020-06-12 17:44:41 +0300

    Create Checkout Page

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -15,3 +15,15 @@ export const selectCartItemsCount = createSelector(
       0
     )
 );
+
+export const selectCartHidden = createSelector(
+  [selectCart],
+  (cart) => cart.hidden
+);
+
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+  cartItems.reduce(
+    (accQuantity, cartItem) => accQuantity + cartItem.quantity * cartItem.price,
+    0
+  )
+);
